de70745d1621b3b1a3dc8246fb4a4f3b10f5dfdf
Dezhong Zhuang <didicomputing@gmail.com>
2022-06-30 19:59:36 +0200

    Added all items

---
+++ InformatikProject/src/main/java/me/didi/items/impl/ESSENCE_REAEVER.java
@@ -0,0 +1,22 @@
+package me.didi.items.impl;
+
+import org.bukkit.inventory.ItemStack;
+
+import me.didi.items.CustomItem;
+import me.didi.utilities.BaseStats;
+
+public class ESSENCE_REAEVER extends CustomItem {
+
+	@Override
+	public BaseStats getBaseStats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack getItemStack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
