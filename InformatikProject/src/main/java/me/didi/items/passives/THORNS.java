ef1c0525d77808f9b074d7ce92c94e1c6fae2f75
Dezhong Zhuang <didicomputing@gmail.com>
2022-06-30 20:13:56 +0200

    Added itempassive

---
+++ InformatikProject/src/main/java/me/didi/items/passives/THORNS.java
@@ -0,0 +1,28 @@
+package me.didi.items.passives;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+
+import me.didi.items.ItemPassive;
+
+public class THORNS implements ItemPassive {
+
+	@Override
+	public void runPassive(Event event, Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
