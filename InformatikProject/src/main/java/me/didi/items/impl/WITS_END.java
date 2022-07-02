491a8d50fd5dfcb643eb24a121dac9bd1602bc64
Dezhong Zhuang <didicomputing@gmail.com>
2022-07-02 19:17:50 +0200

    Fixed one item

--- InformatikProject/src/main/java/me/didi/items/impl/WITS_END.java
+++ InformatikProject/src/main/java/me/didi/items/impl/WITS_END.java
@@ -1,5 +1,6 @@
 package me.didi.items.impl;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Material;
@@ -20,14 +21,13 @@ public class WITS_END extends CustomItem {
 
 	@Override
 	public ItemStack getItemStack() {
-		return new ItemBuilder(Material.DIAMOND_SWORD).setDisplayName(ChatColor.GOLD + "Wit's End")
-				.setLore(getLore()).toItemStack();
+		return new ItemBuilder(Material.DIAMOND_SWORD).setDisplayName(ChatColor.GOLD + "Wit's End").setLore(getLore())
+				.toItemStack();
 	}
 
 	@Override
 	public List<ItemPassive> getItemPassives() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return Arrays.asList();
+	};
 
 }
